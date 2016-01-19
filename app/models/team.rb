a8da3948c52d12eed2b8704b73d4922724e5baf4
Aaron Dawson <adawson927@gmail.com>
2016-01-19 17:50:42 -0500

    more styling, added search function for players page

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,9 @@
 class Team < ActiveRecord::Base
   validates :city, :name, :league, :division, :stadium, :capacity, presence: true
   has_many :players
+
+  def full_name
+    city + ' ' + name
+  end
+
 end
