f0961907a885ec0ae8863042a3aaac9da9a8a647
Aaron Dawson <adawson927@gmail.com>
2016-01-23 14:40:44 -0500

    users can sign up

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :players
   resources :teams
+  resources :users
 
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
