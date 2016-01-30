3a6bcbb4fb8ca2a592235718112e658fec13015e
Aaron Dawson <adawson927@gmail.com>
2016-01-29 22:09:51 -0500

    drag players to lineup to add them

--- app/models/team.rb
+++ app/models/team.rb
@@ -30,23 +30,76 @@ class Team < ActiveRecord::Base
                     second_base, third_base, shortstop,
                     left_field, center_field, right_field,
                     bench1, bench2, bench3, bench4]
-    id = positions[index]
+    if index.nil?
+      '--'
+    else
+      id = positions[index]
+    end
+
     if Player.exists?(id)
       player_at_position = Player.find(id)
     else
-      player_at_position = ''
+      '--'
     end
     player_at_position
   end
 
-  def find_position(player_id)
-    position_ids = [designated_hitter, catcher, first_base,
-                    second_base, third_base, shortstop,
-                    left_field, center_field, right_field,
-                    bench1, bench2, bench3, bench4]
-    position_names = ['DH', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF',
-                      'BN', 'BN', 'BN', 'BN']
-    position_names[position_ids.index(player_id)]
+  def find_batting_order(lineup_position)
+    lineup_ids = [lineup1, lineup2, lineup3, lineup4,
+                  lineup5, lineup6, lineup7, lineup8,
+                  lineup9]
+    if Player.exists?(lineup_ids[lineup_position])
+      Player.find(lineup_ids[lineup_position])
+    else
+      nil
+    end
+  end
+
+  def find_bench(bench_position)
+    bench_ids = [bench1, bench2, bench3, bench4]
+    if Player.exists?(bench_ids[bench_position])
+      Player.find(bench_ids[bench_position])
+    else
+      nil
+    end
+  end
+
+  def find_batting_order_id(lineup_position)
+    lineup_ids = [lineup1, lineup2, lineup3, lineup4,
+                  lineup5, lineup6, lineup7, lineup8,
+                  lineup9]
+    if Player.exists?(lineup_ids[lineup_position])
+      Player.find(lineup_ids[lineup_position]).id
+    else
+      nil
+    end
+  end
+
+  def find_position(lineup_position)
+    lineup_ids = [lineup1, lineup2, lineup3, lineup4,
+                  lineup5, lineup6, lineup7, lineup8,
+                  lineup9]
+                  puts "lineup_ids: #{lineup_ids}"
+    id = lineup_ids[lineup_position]
+    puts "finding player at lineup position #{lineup_position}"
+    puts "#{id} is the id "
+    if id.nil?
+      '--'
+    else
+      position_ids = [designated_hitter, catcher, first_base,
+                      second_base, third_base, shortstop,
+                      left_field, center_field, right_field,
+                      bench1, bench2, bench3, bench4]
+      position_names = ['DH', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF',
+                        'BN', 'BN', 'BN', 'BN']
+      player_id = position_ids.index(id)
+      if player_id.nil?
+        '--'
+      else
+        position_names[player_id]
+      end
+    end
+
   end
 
 
