dda3b7e0ce6440d3583d7e63833fe9c678315a42
Aaron Dawson <adawson927@gmail.com>
2016-08-04 09:11:50 -0400

    fixed issue where currency wasnt displaying properly because of toLocaleString not working in Safari browser

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require turbolinks
 //= require jquery-ui
 //= require_tree .
+
+function localeString(x, sep, grp) {
+  var sx = (''+x).split('.'), s = '', i, j;
+  sep || (sep = ' '); // default seperator
+  grp || grp === 0 || (grp = 3); // default grouping
+  i = sx[0].length;
+  while (i > grp) {
+      j = i - grp;
+      s = sep + sx[0].slice(j, i) + s;
+      i = j;
+  }
+  s = sx[0].slice(0, i) + s;
+  sx[0] = s;
+  return sx.join('.')
+}
